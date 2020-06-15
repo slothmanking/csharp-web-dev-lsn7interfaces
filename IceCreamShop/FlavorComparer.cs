c9f065a2809a8aa404084ccf2d55709e0f4d95da
Jed Morris <jemorris110@gmail.com>
2020-06-15 13:15:47 -0700

    added FlavorComparer

---
+++ IceCreamShop/FlavorComparer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace IceCreamShop
+{
+    public class FlavorComparer : IComparer<Flavor>
+    {
+
+        public int Compare(Flavor x, Flavor y)
+        {
+            return string.Compare(x.Name, y.Name);
+        }
+
+        public int Compare([AllowNull] Flavor x, [AllowNull] Flavor y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
